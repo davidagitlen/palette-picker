29f0e3faca1295dfde6dd268f0dfcc07609c0c5b
davidagitlen <davidagitlen@gmail.com>
2019-10-13 12:36:30 -0600

    Adjust display styling and add lock/unlock icons, hex code display

--- src/containers/Display/Display.js
+++ src/containers/Display/Display.js
@@ -1,16 +1,35 @@
 import React from 'react';
 import './Display.css';
+import locked from '../../images/locked.svg';
+import unlocked from '../../images/unlocked.svg';
 
 const Display = ({ currentPalette, toggleLock }) => {
-  const swatches = currentPalette.colors.map((color, i) => 
-      <button 
-      className='swatch'
-      style={{backgroundColor:color[`hex_${i+1}`]}}
-      key={i + Date.now()}
-      onClick={() => toggleLock(i)}
-      >
-      </button>
-    )
+  const swatches = currentPalette.colors.map((color, i) => {
+  const lockPath = color.locked ? locked : unlocked;
+      return(
+      <>
+        <div 
+          className='swatch'
+          style={{backgroundColor:color[`hex_${i+1}`]}}
+          key={i + Date.now()}
+          >
+        </div>
+        <input 
+          className='lock-image'
+          type='image' 
+          alt='Lock/Unlock'
+          key={'lock' + Date.now()}
+          src={lockPath}
+          onClick={() => toggleLock(i)}
+          />
+        <h3
+          className='hex-code'
+        >
+        {color[`hex_${i + 1}`]}
+        </h3>
+      </>
+      )
+  })
   return (
     <section className="Display">
       <h2>Display</h2>
