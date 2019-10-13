5f6e02dd305e90219ac54e19c61d5a735409f6ec
Katie Williams <kawilliams8@gmail.com>
2019-10-13 15:14:40 -0600

    Add initial styling for Projects and child components, with basic styling and data/function passing

--- src/containers/Project/Project.js
+++ src/containers/Project/Project.js
@@ -2,13 +2,21 @@ import React from 'react';
 import './Project.css';
 import Palettes from '../Palettes/Palettes';
 
-const Project = () => {
+const Project = ({project, palettes, editProject, editPalette, trashProject, trashPalette}) => {
+  const filteredPalettes = palettes.filter(palette => palette.project_id === project.id)
   return (
     <article className="Project">
-      <h3>Project</h3>
-      <Palettes />
+      <div className="project-wrapper">
+        <h3>Project:</h3>
+        <h3>{project.project}</h3>
+      </div>
+      <Palettes
+        palettes={filteredPalettes}
+        editPalette={editPalette}
+        trashPalette={trashPalette}
+      />
     </article>
-  )
+  );
 }
 
 export default Project;
